cc111a8ed622efba47191ef8fe7554fb66c8ada6
GABRIEL GONCALVES DE AVILA MIRANDA <164177408+gab8485@users.noreply.github.com>
2025-12-14 14:30:54 -0300

    Add files via upload

---
+++ Frontend/src/app/app.routes.ts
@@ -0,0 +1,20 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { SignUpComponent } from './pages/signup/signup.component';
+import { AuthGuard } from './services/auth-guard.service';
+
+export const routes: Routes = [
+    {
+        path: "login",
+        component: LoginComponent
+    },
+    {
+        path: "signup",
+        component: SignUpComponent
+    }
+     {
+        path: "user",
+        component: SignUpComponent,
+        canActivate: [AuthGuard]
+    }
+];
